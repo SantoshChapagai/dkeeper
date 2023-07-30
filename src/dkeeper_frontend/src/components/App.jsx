851eed7c289584970d2cbb69f8a2eb0750a46c28
SantoshChapagai <santoshchapagai100@gmail.com>
2023-07-30 17:06:35 +0300

    adding and retrive data from canister

--- src/dkeeper_frontend/src/components/App.jsx
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeeper } from "../../../declarations/dkeeper_backend";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+      dkeeper.createNote(newNote.title, newNote.content)
+      return [newNote, ...prevNotes];
     });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray);
   }
 
   function deleteNote(id) {
